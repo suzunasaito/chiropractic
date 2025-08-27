a7b08bd49350b5b11814f48d63e9088b842b9563
Suzuna Saito <suzuna.saito@vanarts.com>
2025-08-27 11:22:14 -0700

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { TreatmentsComponent } from './treatments/treatments.component';
+import { LocationComponent } from './location/location.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent},
+    { path: 'about', component: AboutComponent},
+    { path: 'treatments', component: TreatmentsComponent},
+    { path: 'location', component: LocationComponent}
+];
