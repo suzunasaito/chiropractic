99f41b4292e235ab4a7ba365eb12889c44404938
Suzuna Saito <suzuna.saito@vanarts.com>
2025-10-30 13:06:42 -0700

    image size changed

--- dist/chiropractic/server/angular-app-manifest.mjs
+++ dist/chiropractic/server/angular-app-manifest.mjs
@@ -24,12 +24,12 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 13835, hash: '62b20e21ff382ed27032a01847ab8ebe32e453b753e53f17eaa7b0c90a5222d5', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 14348, hash: '86bcc2dbfb39c8d082630744b98238c47751fc398a8fcdab1ea70526a66cd273', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'about/index.html': {size: 25143, hash: '4793b15f23ad4d25a08e4e5e6aef47c666b7440a2710acd040e12eee74f4f386', text: () => import('./assets-chunks/about_index_html.mjs').then(m => m.default)},
-    'treatments/index.html': {size: 25728, hash: '813d0f1165c46668bca9fbb18e92f6cb27c98ac9f30c2c2b85e82340f245eda7', text: () => import('./assets-chunks/treatments_index_html.mjs').then(m => m.default)},
-    'index.html': {size: 31355, hash: '9779f917248756e3b758120b796c52f66119c62f231af22dcf597078923b0b03', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
-    'location/index.html': {size: 20703, hash: 'fa0ce664118928b72ead08b3e793774e546f0c993e999a0a4c57e6ea3e8af440', text: () => import('./assets-chunks/location_index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 13835, hash: '94fc09bae8a347cc838c8baa1614b057efdb828eef0c5beee6ff3ac84a112ba6', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 14348, hash: 'e4e5443cdd315ea9f944bc52565a3c0382235a2ad0a3aca131161dd3eda85229', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'about/index.html': {size: 25143, hash: 'b79d533557005e1a838938eef61c8addb29d8328e91679270d87ccbd189c7dcb', text: () => import('./assets-chunks/about_index_html.mjs').then(m => m.default)},
+    'index.html': {size: 31332, hash: 'bd43b97047d4cd000c83c14873bdcec9814087c5d75583a947c266ed1c1a6139', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'location/index.html': {size: 20703, hash: '67a537d5c3ad1dab0462c6dac7170daef5e2290085e76e136fe3d99cc570f5b9', text: () => import('./assets-chunks/location_index_html.mjs').then(m => m.default)},
+    'treatments/index.html': {size: 25728, hash: '42c720eb9dc6ae4b8d249190667e348747c60dc8294c670400548f9cc01944a8', text: () => import('./assets-chunks/treatments_index_html.mjs').then(m => m.default)},
     'styles-5INURTSO.css': {size: 0, hash: 'menYUTfbRu8', text: () => import('./assets-chunks/styles-5INURTSO_css.mjs').then(m => m.default)}
   },
 };
