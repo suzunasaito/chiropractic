e7b57de87901ff256e6bc9d01889f90f5908124a
Suzuna Saito <suzuna.saito@vanarts.com>
2025-11-05 12:45:13 -0800

    nav changed

--- dist/chiropractic/server/angular-app-manifest.mjs
+++ dist/chiropractic/server/angular-app-manifest.mjs
@@ -24,12 +24,12 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 13949, hash: 'c4d6f77b1146d05ca60213c698f9a3e44a73bf8d276479c185bed75f2dcbf1f2', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 14351, hash: '5286a2179dc378e6041c2521ccc88b5d085dd8d35d2da85c1e7d5c6f5ac604a1', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'treatments/index.html': {size: 23801, hash: '3f9c0abb5a04eb82f4125c2de8bcb45e4d91f61463d37f3579829040ae6a65d4', text: () => import('./assets-chunks/treatments_index_html.mjs').then(m => m.default)},
-    'location/index.html': {size: 20784, hash: 'f57ea8ff32d51606c7ffb62cada7bb120562d01b899d7ddf5d8cf0e977303c7e', text: () => import('./assets-chunks/location_index_html.mjs').then(m => m.default)},
-    'about/index.html': {size: 23274, hash: '178aa345e2c3bd830b8ac1082b534cbc76eef54b34d5c383f2bbe8ba7066b3a7', text: () => import('./assets-chunks/about_index_html.mjs').then(m => m.default)},
-    'index.html': {size: 30249, hash: 'b8a4666d0c695834245b4fb8d69d91e2663dd3db3c15fbe8a9c945894a2015f9', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 13949, hash: '5aaef97b0bcf1d2255a61c19457778dd7aab2ccae8567eefdc6af20a38f2eb38', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 14351, hash: '6aecbfac285f0589fa8aba850ea504520edb1512a843df38960bbb60b056cffc', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'treatments/index.html': {size: 23801, hash: '01426c5cbeeb7c394e4037bd93928ca3c5c2c035e7e46751d8b8af2f8921c345', text: () => import('./assets-chunks/treatments_index_html.mjs').then(m => m.default)},
+    'location/index.html': {size: 20784, hash: '48852e71635f2d92792a12493d1a990c7b999c3b0119af0706bf38c4117a2bfc', text: () => import('./assets-chunks/location_index_html.mjs').then(m => m.default)},
+    'index.html': {size: 30249, hash: '2b58e6a17c41aee424fabefd251c528ac24fbcb2c32c58a67ed5254828aed3f6', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'about/index.html': {size: 23274, hash: 'a5c16a6e4977159a18dc7874ee532ceed1ce8660bb5bd64b82fd9ea81d78d44f', text: () => import('./assets-chunks/about_index_html.mjs').then(m => m.default)},
     'styles-5AXVTDPM.css': {size: 1921, hash: 'MIUfX9aE2eg', text: () => import('./assets-chunks/styles-5AXVTDPM_css.mjs').then(m => m.default)}
   },
 };
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,12 @@ import { RouterModule, RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'chiropractic';
+
+  closeNavbar() {
+    const navbar = document.querySelector('.navbar-collapse.show') as HTMLElement;
+    if (navbar) {
+      const collapse = new (window as any).bootstrap.Collapse(navbar, { toggle: false });
+      collapse.hide();
+    }
+  }
 }
